f534427ef9f20a2be5b307034256fe506860388d
Mike Wyatt <wyatt.mike@gmail.com>
2023-09-05 21:38:40 -0300

    at least compile

--- src-tauri/src/datasource/mod.rs
+++ src-tauri/src/datasource/mod.rs
@@ -1,3 +1,21 @@
 pub struct DataSource;
+use async_graphql::Context;
+use async_graphql::Result as GraphQLResult;
 
-impl DataSource {}
+use crate::graphql::generated::Game;
+use crate::graphql::generated::PlaySession;
+use crate::graphql::generated::Query;
+
+impl DataSource {
+  pub async fn Game_play_sessions(
+    &self,
+    root: &Game,
+    ctx: &Context<'_>,
+  ) -> GraphQLResult<Vec<PlaySession>> {
+    todo!()
+  }
+
+  pub async fn Query_getGames(&self, root: &Query, ctx: &Context<'_>) -> GraphQLResult<Vec<Game>> {
+    todo!()
+  }
+}
