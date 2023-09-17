cc08f07c23fdd76fa18502d56e7fd89e6988ea13
Mike Wyatt <wyatt.mike@gmail.com>
2023-09-17 00:24:48 -0230

    put initialization behind suspense

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,31 +1,74 @@
-import { useMutation } from '@apollo/client'
+import { FetchResult, useMutation } from '@apollo/client'
 import { CircularProgress } from '@mui/joy'
 import GameList from '@src/games/GameList'
-import { InitializeAppDocument } from '@src/graphql/operations'
-import { useEffect, useState } from 'react'
+import {
+  InitializeAppDocument,
+  InitializeAppMutation,
+} from '@src/graphql/operations'
+import { PropsWithChildren, Suspense, useEffect, useState } from 'react'
 
 function App() {
-  // TODO Can this be done with suspense? Might have to use a query to get easy
-  // suspense ...
-  const [isLoaded, setIsLoaded] = useState(false)
+  return (
+    <Suspense fallback={<CircularProgress />}>
+      <Initializer>
+        <GameList />
+      </Initializer>
+    </Suspense>
+  )
+}
+
+function Initializer(props: PropsWithChildren<{}>) {
   const [initializeApp] = useMutation(InitializeAppDocument)
+  // const [wrappedPromise] = useState(() => {
+  //   return wrapPromiseForSuspense(initializeApp())
+  // })
+  const [wrappedPromise, setWrappedPromise] =
+    useState<SuspenseWrappedPromise<FetchResult<InitializeAppMutation>>>()
 
   useEffect(() => {
-    async function run() {
-      await initializeApp()
-      setIsLoaded(true)
-    }
-
-    run()
+    setWrappedPromise(wrapPromiseForSuspense(initializeApp()))
   }, [initializeApp])
 
-  return isLoaded ? (
-    <GameList />
-  ) : (
-    <>
-      <CircularProgress />
-    </>
+  return (
+    <>{wrappedPromise?.read()?.status === 'success' ? props.children : null}</>
   )
+}
+
+type WrappedPromiseStatus = 'pending' | 'success' | 'error'
+
+interface SuspenseWrappedPromise<T> {
+  read: () => { result: T; status: WrappedPromiseStatus } | undefined
+}
+
+function wrapPromiseForSuspense<T>(promise: Promise<T>) {
+  let status: 'pending' | 'success' | 'error' = 'pending'
+  let result: T
+  let err: any
+
+  const suspender = promise.then(
+    (value) => {
+      status = 'success'
+      result = value
+    },
+    (e) => {
+      status = 'error'
+      err = e
+    },
+  )
+
+  const wrappedPromise: SuspenseWrappedPromise<T> = {
+    read() {
+      if (status === 'pending') {
+        throw suspender
+      } else if (status === 'error') {
+        throw err
+      } else if (status === 'success') {
+        return { status, result }
+      }
+    },
+  }
+
+  return wrappedPromise
 }
 
 export default App
