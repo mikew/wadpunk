06f3506ea9f933a3ead28f38b4d04047d8a2386c
Mike Wyatt <wyatt.mike@gmail.com>
2023-09-17 00:25:11 -0230

    more dialog stuff

--- src/games/GameDialog.tsx
+++ src/games/GameDialog.tsx
@@ -1,4 +1,4 @@
-import { useMutation } from '@apollo/client'
+import { useMutation, useQuery } from '@apollo/client'
 import {
   Button,
   FormControl,
@@ -8,50 +8,103 @@ import {
   ModalDialog,
   Textarea,
   Typography,
+  DialogTitle,
+  DialogActions,
+  DialogContent,
+  List,
+  ListItem,
+  ListItemContent,
+  ListItemDecorator,
+  Checkbox,
 } from '@mui/joy'
-import { UpdateNotesDocument } from '@src/graphql/operations'
-import { forwardRef } from 'react'
+import { forwardRef, useMemo } from 'react'
+
+import {
+  GetGameFilesDocument,
+  UpdateNotesDocument,
+} from '@src/graphql/operations'
+
 import { type GameListGame } from './GameList'
 
 const GameDialog = forwardRef<HTMLDivElement, { game: GameListGame }>(
   (props, ref) => {
+    const iwad = 'Doom 2/'
+    // const iwad = undefined
+
+    const game_ids = useMemo(() => {
+      const ids = [props.game.id]
+
+      if (iwad) {
+        ids.unshift(iwad)
+      }
+
+      return ids
+    }, [iwad, props.game.id])
+
     const [updateNotes] = useMutation(UpdateNotesDocument)
+    const { data: gameFiles } = useQuery(GetGameFilesDocument, {
+      variables: {
+        game_ids,
+      },
+    })
 
     return (
-      <ModalDialog
-        aria-labelledby="size-modal-title"
-        aria-describedby="size-modal-description"
-        size="sm"
-        ref={ref}
-      >
-        <ModalClose />
-
-        <Typography id="size-modal-title" level="h2">
+      <ModalDialog ref={ref}>
+        <DialogTitle>
           {props.game.name}
-        </Typography>
+          <ModalClose />
+        </DialogTitle>
 
-        <Typography id="size-modal-description">
-          {props.game.description}
-        </Typography>
+        <DialogContent>
+          <Typography id="size-modal-description">
+            {props.game.description}
+          </Typography>
 
-        <FormControl>
-          <FormLabel>Notes</FormLabel>
-          <Textarea minRows={2} value={props.game.notes} />
-          <FormHelperText>This is a helper text.</FormHelperText>
-        </FormControl>
+          <FormLabel>Files</FormLabel>
+          <List size="sm" variant="outlined">
+            {gameFiles?.getGameFiles.map((x) => {
+              return (
+                <ListItem key={x}>
+                  <ListItemDecorator>
+                    <Checkbox
+                      size="sm"
+                      checked={
+                        x.toLowerCase().endsWith('.wad') ||
+                        x.toLowerCase().endsWith('pk3') ||
+                        x.toLowerCase().endsWith('.iwad')
+                      }
+                    />
+                  </ListItemDecorator>
+                  <ListItemContent>{x}</ListItemContent>
+                </ListItem>
+              )
+            })}
+          </List>
 
-        <Button
-          onClick={() => {
-            updateNotes({
-              variables: {
-                game_id: props.game.id,
-                notes: 'updated from UI',
-              },
-            })
-          }}
-        >
-          Save
-        </Button>
+          <FormControl>
+            <FormLabel>Notes</FormLabel>
+            <Textarea minRows={2} value={props.game.notes} />
+            {/* <FormHelperText>This is a helper text.</FormHelperText> */}
+          </FormControl>
+        </DialogContent>
+
+        <DialogActions>
+          <Button>Play</Button>
+
+          <Button
+            onClick={() => {
+              updateNotes({
+                variables: {
+                  game_id: props.game.id,
+                  notes: 'updated from UI',
+                },
+              })
+            }}
+            color="neutral"
+          >
+            Save
+          </Button>
+        </DialogActions>
       </ModalDialog>
     )
   },
