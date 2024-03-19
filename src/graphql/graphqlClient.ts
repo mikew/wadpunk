07605b70c0151bd341173280f51be11aab17aa73
Mike Wyatt <wyatt.mike@gmail.com>
2024-03-19 10:12:53 -0300

    add invalidateApolloQuery

--- src/graphql/graphqlClient.ts
+++ src/graphql/graphqlClient.ts
@@ -1,12 +1,31 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client'
 
 import tauriGraphqlApolloLink from '#src/tauri/tauriGraphqlApolloLink'
+
+import type { Query } from './types'
 // import tauriGraphqlHttpLink from '#src/tauri/tauriGraphqlHttpLink'
 
+const apolloCache = new InMemoryCache()
+
 const graphqlClient = new ApolloClient({
-  cache: new InMemoryCache(),
+  cache: apolloCache,
   link: tauriGraphqlApolloLink,
   // link: tauriGraphqlHttpLink,
 })
+
+export const invalidateApolloQuery = (queryNames: (keyof Query)[]) => {
+  const response = queryNames.map((queryName) =>
+    apolloCache.evict({
+      id: 'ROOT_QUERY',
+      fieldName: queryName,
+    }),
+  )
+
+  // See note towards the end of
+  // https://www.apollographql.com/docs/react/caching/garbage-collection/#cacheevict
+  apolloCache.gc()
+
+  return response
+}
 
 export default graphqlClient
