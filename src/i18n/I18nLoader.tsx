8f274dff8815e2c02f0e2423d0502469ca454b15
Mike Wyatt <wyatt.mike@gmail.com>
2024-04-14 13:50:41 -0300

    make i18n suspenseful

---
+++ src/i18n/I18nLoader.tsx
@@ -0,0 +1,53 @@
+import type { Dict } from 'i18n-js'
+import { useEffect, useRef, useState } from 'react'
+
+import {
+  wrapPromiseForSuspense,
+  type SuspenseWrappedPromise,
+} from '#src/lib/wrapPromiseForSuspense'
+
+import i18nConfig from './config'
+import I18nProvider from './lib/I18nProvider'
+import normalizeLocale from './lib/normalizeLocale'
+import prepareSupportedLocales from './lib/prepareSupportedLocales'
+
+const I18nLoader: React.FC<React.PropsWithChildren> = (props) => {
+  const i18nRef = useRef<{ currentLocale: string; translations: Dict }>()
+  const [wrappedPromise, setWrappedPromise] =
+    useState<SuspenseWrappedPromise<unknown>>()
+
+  useEffect(() => {
+    async function run() {
+      await prepareSupportedLocales()
+      const currentLocale = normalizeLocale(
+        localStorage.getItem(i18nConfig.cookieName),
+      )
+      const translations = await i18nConfig.loadTranslations(currentLocale)
+
+      i18nRef.current = {
+        currentLocale,
+        translations,
+      }
+    }
+
+    setWrappedPromise(wrapPromiseForSuspense(run()))
+  }, [])
+
+  return (
+    <>
+      {wrappedPromise?.read().status === 'success' && i18nRef.current ? (
+        <>
+          <I18nProvider
+            supportedLocales={i18nConfig.supportedLocales || []}
+            locale={i18nRef.current.currentLocale}
+            translations={i18nRef.current.translations}
+          >
+            {props.children}
+          </I18nProvider>
+        </>
+      ) : null}
+    </>
+  )
+}
+
+export default I18nLoader
