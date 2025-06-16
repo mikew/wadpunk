a1e7db90c85f4158e2cdfb42d7f45c53253c0408
Mike Wyatt <wyatt.mike@gmail.com>
2025-06-16 19:12:15 -0300

    fixes

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,6 +2,8 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri::Manager;
+
 use graphql::datasource::DataSource;
 
 mod database;
