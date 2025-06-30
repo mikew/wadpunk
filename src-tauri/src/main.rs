7ca95f6e6b18866a3bae1da7e748e939fa3d8c2e
Mike Wyatt <wyatt.mike@gmail.com>
2025-06-30 19:57:41 -0300

    WIP

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,7 +4,7 @@
 
 use tauri::Emitter;
 use tauri::Manager;
-use tauri_plugin_cli::CliExt;
+// use tauri_plugin_cli::CliExt;
 
 use graphql::datasource::DataSource;
 
@@ -27,6 +27,11 @@ fn main() {
   tauri::Builder::default()
     .setup(|app| {
       crate::tauri_legacy::set_app_handle(app.handle().clone());
+			#[cfg(any(windows, target_os="linux"))]
+{
+use tauri_plugin_deep_link::DeepLinkExt;
+let _ = app.deep_link().register_all();
+}
       Ok(())
     })
     .plugin(tauri_plugin_single_instance::init(|app, args, _cwd| {
@@ -37,15 +42,15 @@ fn main() {
           .expect("no main window")
           .set_focus();
 
-        let matches = app.cli().matches_from(args);
-        match matches {
-          Ok(matches) => {
-            let _ = app.emit_to("main", "cli", matches);
-          }
-          Err(e) => {
-            eprintln!("Error parsing CLI arguments: {}", e);
-          }
-        }
+        // let matches = app.cli().matches_from(args);
+        // match matches {
+        //   Ok(matches) => {
+        //     let _ = app.emit_to("main", "cli", matches);
+        //   }
+        //   Err(e) => {
+        //     eprintln!("Error parsing CLI arguments: {}", e);
+        //   }
+        // }
       }
     }))
     // break
@@ -56,8 +61,8 @@ fn main() {
     .plugin(tauri_plugin_opener::init())
     .plugin(tauri_plugin_process::init())
     .plugin(tauri_plugin_shell::init())
-    .plugin(tauri_plugin_cli::init())
     .plugin(tauri_plugin_deep_link::init())
+    // .plugin(tauri_plugin_cli::init())
     // break
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
--- src/tauri/TauriCliHandler.tsx
+++ src/tauri/TauriCliHandler.tsx
@@ -1,6 +1,7 @@
 import { listen } from '@tauri-apps/api/event'
 import type { CliMatches } from '@tauri-apps/plugin-cli'
 import { getMatches } from '@tauri-apps/plugin-cli'
+import { onOpenUrl } from '@tauri-apps/plugin-deep-link'
 import { useEffect, useRef } from 'react'
 
 import { actions } from '#src/games/redux'
@@ -59,6 +60,44 @@ function TauriCliHandler() {
     }
 
     run()
+
+    return () => {
+      listening = false
+    }
+  }, [dispatch, startGame])
+
+  useEffect(() => {
+    let listening = true
+
+    onOpenUrl(async (urls) => {
+      if (!listening) {
+        return
+      }
+
+      for (const url of urls) {
+        const parsed = new URL(url)
+        console.log(parsed)
+        if (parsed.protocol !== 'wadpunk:') {
+          continue
+        }
+
+        switch (parsed.host) {
+          case 'launch-game': {
+            const args = parsed.pathname.split('/').slice(1)
+            const gameId = args[0]
+
+            console.log(gameId)
+
+            if (gameId) {
+              dispatch(actions.setSelectedId(gameId))
+              startGame(gameId)
+            }
+          }
+        }
+      }
+
+      console.log(urls)
+    })
 
     return () => {
       listening = false
