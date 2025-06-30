45062ca10e9a61eb49ffabee745a3abcbc29aa4a
Mike Wyatt <wyatt.mike@gmail.com>
2025-06-30 18:17:30 -0300

    WIP gonna switch to linux for this because development will be easier

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -48,15 +48,17 @@ fn main() {
         }
       }
     }))
+    // break
     .plugin(tauri_plugin_window_state::Builder::default().build())
     .plugin(tauri_plugin_graphql::init(schema))
     .plugin(tauri_plugin_updater::Builder::new().build())
-
+    // break
     .plugin(tauri_plugin_opener::init())
     .plugin(tauri_plugin_process::init())
     .plugin(tauri_plugin_shell::init())
     .plugin(tauri_plugin_cli::init())
-
+    .plugin(tauri_plugin_deep_link::init())
+    // break
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
